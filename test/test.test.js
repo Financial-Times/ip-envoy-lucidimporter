5e2e8fa8d38d224b93cb1f8cb18dcdd80aa2cdb1
sentayhuJo <sentayhu.alim@mail.com>
2019-06-20 18:00:30 +0100

    set time to exit  🐿 v2.12.3

--- test/test.test.js
+++ test/test.test.js
@@ -1,6 +1,11 @@
 const { init } = require('../cron');
 const knex = require('../db/connect')
 
+function delay(ms) {
+  console.log(`Waiting for ${ms} milliseconds...`);
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 describe('Test importer ', () => {
   beforeAll(() => {
     init();
@@ -11,6 +16,7 @@ describe('Test importer ', () => {
     done();
   })
   afterAll(async (done) => {
+    delay(5);
     knex.destroy();
     done();
   })
