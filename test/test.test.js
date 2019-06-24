ecbb0aaf9bcd2a11370551feae54e40c5486a267
sentayhuJo <sentayhu.alim@mail.com>
2019-06-24 12:06:29 +0100

    testing  🐿 v2.12.3

--- test/test.test.js
+++ test/test.test.js
@@ -2,34 +2,31 @@ const waitForExpect = require('wait-for-expect');
 const { initialise } = require('../db/initialise');
 const knex = require('../db/connect')
 
-jest.setTimeout(30000);
-
 function delay(ms) {
   console.log(`Waiting for ${ms} milliseconds...`);
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function searchSilos(testFunctions) {
-  await waitForExpect(async () => {
-    const query = await knex.raw(`
-      SELECT silo.name AS "siloName" from core.entity_silo as es
-      LEFT join core.silo as silo
-      ON silo."siloId" = es."siloId"
-      `);
-    const siloNames = query.rows.map((row) => row.siloName);
-    testFunctions(siloNames);
-  }, 30000, 1000);
+  const query = await knex.raw(`
+    SELECT silo.name AS "siloName" from core.entity_silo as es
+    LEFT join core.silo as silo
+    ON silo."siloId" = es."siloId"
+    `);
+  const siloNames = query.rows.map((row) => row.siloName);
+  console.log('names: ', siloNames )
+  testFunctions(siloNames);
 }
 
 describe('Test importer ', () => {
   beforeAll(async (done) => {
+    jest.setTimeout(30000);
     await initialise();
     done();
   })
 
   it('it should return silo names containing SOURCE and DRAIN ', async done => {
-    await searchSilos((siloNames) => {
-      console.log(siloNames);
+    await searchSilos(siloNames => {
       expect(siloNames).toEqual(expect.arrayContaining(['Source', 'Drain']));
     });
     done();
