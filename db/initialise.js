877e67b3cc77d7539b9cd8893a9249080b99c1f0
sentayhuJo <sentayhu.alim@mail.com>
2019-06-24 15:47:16 +0100

    add new test  🐿 v2.12.3

--- db/initialise.js
+++ db/initialise.js
@@ -7,7 +7,7 @@ const importFromLucid = promisify(importFromLucidchart);
 async function initialise() {
   console.debug('*** 1 - build up database... ***');
   await init();
-    if (await importFromLucid('test')) {
+    if (await importFromLucid('my_ft_welcome_v2')) {
       console.debug('*** 4 - New tracks imported ***');
     }
   console.debug('Ready.');
