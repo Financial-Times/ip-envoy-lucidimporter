cb5d3f2b828e73183f9b98f4f260c498d652c94e
John Coxon <j@coxons.com>
2019-08-30 12:39:57 +0100

    Fixes lint errors in tests  🐿 v2.12.4

--- test/behavioral.test.js
+++ test/behavioral.test.js
@@ -1,13 +1,13 @@
 const waitForExpect = require('wait-for-expect');
 const { initialise } = require('../service/initialise');
-const knex = require('../service/connect')
+const knex = require('../service/connect');
 
 function delay(ms) {
   console.log(`Waiting for ${ms} milliseconds...`);
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function queryFactory(testQuery , testFunctions) {
+async function queryFactory(testQuery, testFunctions) {
   await waitForExpect(async () => {
     const query = await knex.raw(`${testQuery}`);
     testFunctions(query.rows);
@@ -20,22 +20,22 @@ describe('Test Lucid chart Importer ', () => {
     done();
   });
 
-  it('it should return track name TEST and Track Status 2', async done => {
+  it('it should return track name TEST and Track Status 2', async (done) => {
     const query = 'SELECT core.track.name, core.track."trackStatusId" FROM core.track;';
     const expected = {
       name: 'test',
       trackStatusId: 2
     };
-    await queryFactory(query, track => {
+    await queryFactory(query, (track) => {
       expect(track[0]).toEqual(expected);
     });
     done();
   });
-  
-  it('it should return silo Source, Stage1, Stage 2 and shelf', async done => {
+
+  it('it should return silo Source, Stage1, Stage 2 and shelf', async (done) => {
     const query = 'SELECT core.silo.name FROM core.silo;';
-    await queryFactory(query, siloNames => {
-      const names = siloNames.map(silo => silo.name);
+    await queryFactory(query, (siloNames) => {
+      const names = siloNames.map((silo) => silo.name);
       expect(names).toEqual(expect.arrayContaining(['Source', 'Stage 1', 'Stage 2', 'Shelf']));
     });
     done();
