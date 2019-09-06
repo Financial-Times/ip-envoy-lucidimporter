461ef5de311e6846e917655b25ebfe1a588587df
John Coxon <j@coxons.com>
2019-09-06 15:44:19 +0100

    Fixes test error and corrects some lint problems  🐿 v2.12.4

--- service/initialise.js
+++ service/initialise.js
@@ -7,9 +7,9 @@ const importFromLucid = promisify(importFromLucidchart);
 async function initialise() {
   console.debug('*** 1 - build up test database... ***');
   await init();
-    if (await importFromLucid('test')) {
-      console.debug('*** 4 - New tracks imported ***');
-    }
+  if (await importFromLucid('test')) {
+    console.debug('*** 4 - New tracks imported ***');
+  }
   console.debug('Ready.');
 }
 
