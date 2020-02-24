67f14cc7d338d52ccf1bf1987ff5ea5f16249cfd
sentayhu.mekoonnali <sentayhu.mekoonnali@FT-MW4737.local>
2020-02-24 13:58:10 +0000

    Define initial lucid data import function 🐿 v2.12.5

--- service/initialise.js
+++ service/initialise.js
@@ -1,13 +1,17 @@
 
 const { promisify } = require('util');
-const { init, importFromLucidchart } = require('./index');
+const path = require('path');
+const { init } = require('./index');
+const { initiateImport } = require('../src');
+const knex = require('./connect');
 
-const importFromLucid = promisify(importFromLucidchart);
+const initiateLucidImport = promisify(initiateImport);
 
 async function initialise() {
   console.debug('*** 1 - build up test database... ***');
+  const jouneyFilePath = path.resolve(__dirname,'../data/test.csv');
   await init();
-  if (await importFromLucid('test')) {
+  if (await initiateLucidImport(jouneyFilePath, knex)) {
     console.debug('*** 4 - New tracks imported ***');
   }
   console.debug('Ready.');
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const { preParser } = require('../lib/parser');
 const { dbBuilder } = require('../lib/builder');
+const { initiateImport } = require('../lib/initiateImport');
 const { isViolateRule } = require('../lib/isViolateRule');
 
 module.exports = {
   preParser,
   dbBuilder,
-  isViolateRule
+  isViolateRule,
+  initiateImport
 };
